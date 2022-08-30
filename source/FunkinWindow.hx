03508c141933abf46a8dbd7b659e7ad1e3ce30e7
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2022-08-30 03:01:20 -0400

    debhjgjklasdgjnbasdjng

--- source/FunkinWindow.hx
+++ source/FunkinWindow.hx
@@ -9,15 +9,12 @@ class FunkinWindow extends Application
 {
     public static var appName = "Friday Night Funkin': NeonCrusher Engine";
 
-    public static function changeAppName(newAppName:String) {
+    public static function changeAppName(newAppName:String)
         Application.current.window.title = newAppName;
-    }
     
-    public static function changeAppX(newAppX:Int) {
+    public static function changeAppX(newAppX:Int)
         Application.current.window.x = newAppX;
-    }
     
-    public static function changeAppY(newAppY:Int) {
+    public static function changeAppY(newAppY:Int)
         Application.current.window.y = newAppY;
-    }
 }
